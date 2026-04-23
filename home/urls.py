a840379f3609d951de1155ccb80dcb5074a67972
PRINCE-CEEJAY <ceejaydroidprince@gmail.com>
2026-04-23 11:00:34 +0100

    added update and delete and fixed htmx target issues

--- home/urls.py
+++ home/urls.py
@@ -5,5 +5,6 @@ from home import views
 urlpatterns = [
     path('', views.task_list_view, name='task_list'),
     path('create/', views.task_create_view, name='task_create'),
-
+    path('update/<int:id>/', views.task_update, name='task_update'),
+    path('delete/<int:id>/', views.tasK_delete, name='task_delete'),
 ]
--- home/views.py
+++ home/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, HttpResponse
 from .models import Task
 from .forms import Taskform
 from django.contrib.auth.decorators import login_required
@@ -7,7 +7,7 @@ from django.contrib.auth.decorators import login_required
 @login_required(login_url='/admin/')
 def task_list_view(request):
     task_list = Task.objects.filter(user=request.user).all().order_by('-date_created')
-    context = {'data': task_list}
+    context = {'all_tasks': task_list}
     return render(request, 'home/task-list.html', context)
 
 
@@ -19,10 +19,38 @@ def task_create_view(request):
             task = form.save(commit=False)
             task.user = request.user
             task.save()
-            context = {'data': task}
-            return render(request, 'home/task-item.html', context)
+            context = {'task': task }
+            return render(request, 'home/task-row.html', context)
         
-    context = {'form': Taskform()}
-    return render(request, 'home/task-create.html', context)
+    context = {
+        'form': Taskform(),
+        'action_url': '/create/',
+        'swap_type': 'afterbegin'
+        }
+    return render(request, 'home/task-form.html', context)
 
-   
+@login_required(login_url='/admin/')  
+def task_update(request, id):
+    task = get_object_or_404(Task, user = request.user, id=id)
+    if request.method == "POST":
+        form = Taskform(request.POST)
+        if form.is_valid():
+            task_item = form.save(commit=False)
+            task_item.user = request.user
+            task_item.save()
+            context = {'task': task_item}
+            return render(request, 'home/task-row.html', context)
+    else:
+        context = {
+            'form': Taskform(instance = task),
+            'action_url': f"/update/{id}/",
+            'swap_type': 'outerHTML',
+        }
+        return render(request, 'home/task-form.html', context)
+
+
+@login_required(login_url='/admin/')  
+def tasK_delete(request, id):
+    task = get_object_or_404(Task, user=request.user, id=id)
+    task.delete()
+    return HttpResponse('')
